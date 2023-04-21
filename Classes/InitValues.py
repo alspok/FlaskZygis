c59123d28455d4379bf866ddf3c1f726c49267c4
alspok <alspok@gmail.com>
2023-04-21 13:35:00 +0300

    .

--- Classes/InitValues.py
+++ Classes/InitValues.py
@@ -1,7 +1,8 @@
 class InitValues():
     file_list = [
-        "DataFile\MergeFile.filter.csv",
-        "DataFile\MergeFile.mod.csv"
+        "DataFile\\NOVAENGEL_EPTIMO_InventoryReport_20230412191230.csv.merge.filter",
+        "DataFile\\AB_PL_SKORPIONAS_ELKO.csv.merge.filter",
+        "DataFile\\b2b_morele_prodcat_stockexport.csv.merge.filter"
     ]
 
-    file_name = "DataFile\MergeFile.filter.csv"
+    # file_name = "DataFile\MergeFile.filter.csv"
